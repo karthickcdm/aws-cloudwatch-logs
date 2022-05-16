8aa0e83296ede7c8a489068d407857718fc7acbe
Karthick <karthick.k@clsslabs.com>
2022-05-16 10:10:09 +0530

    Initial Commit
    Initial Commit

---
+++ aws-cloudwatchlogs.js
@@ -0,0 +1,40 @@
+var winston = require('winston')
+const CloudWatchTransport = require('winston-aws-cloudwatch');
+
+var NODE_ENV = process.env.NODE_ENV || 'development';
+
+const logger = winston.createLogger({
+  transports: [
+    new (winston.transports.Console)({
+      timestamp: true,
+      colorize: true,
+    })
+  ]
+});
+
+var config = {
+  logGroupName: 'my-log-group',
+  logStreamName: NODE_ENV,
+  createLogGroup: false,
+  createLogStream: true,
+  awsConfig: {
+    accessKeyId: process.env.CLOUDWATCH_ACCESS_KEY_ID,
+    secretAccessKey: process.env.CLOUDWATCH_SECRET_ACCESS_KEY,
+    region: process.env.CLOUDWATCH_REGION
+  },
+  formatLog: function (item) {
+    return item.level + ': ' + item.message + ' ' + JSON.stringify(item.meta)
+  }
+}
+
+if (NODE_ENV == 'development') logger.add(CloudWatchTransport, config);
+
+logger.level = process.env.LOG_LEVEL || "one";
+
+logger.stream = {
+  write: function(message, encoding) {
+    logger.info(message);
+  }
+};
+
+module.exports = logger;
